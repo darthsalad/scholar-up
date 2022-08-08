721a12cd684bf3d0f973c6cd3f43200f68f8106c
DarthSalad <piyushmishra965@gmail.com>
2022-08-08 09:15:55 +0530

    college portal folder structure

--- college-portal/src/App.js
+++ college-portal/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Home from "./pages/Home/Home"
+import Login from "./pages/Auth/Login"
+import Register from "./pages/Auth/Register"
+import Profile from "./pages/Profile/Profile"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+// import { Navbar } from "./components/Navbar"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/Register" element={<Register />} />
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
