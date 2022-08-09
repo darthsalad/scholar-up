f947903a2e3d7320ccbb3e72535c7b7a32a923b2
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-08-09 11:26:16 +0530

    Added stats

--- college-portal/src/pages/Home/Home.jsx
+++ college-portal/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import { StatsGroup } from "../../components/Stats/Stats";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <StatsGroup></StatsGroup>
     </div>
   );
 };
