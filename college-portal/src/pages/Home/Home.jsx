38265014f103232ce6efd20807e1fcb618801603
DarthSalad <piyushmishra965@gmail.com>
2022-08-17 11:40:00 +0530

    searchbar added in verified

--- college-portal/src/pages/Home/Home.jsx
+++ college-portal/src/pages/Home/Home.jsx
@@ -2,12 +2,32 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { StatsGroup } from "../../components/Stats/Stats";
 import NoticeBoard from "../../components/NoticeBoard/NoticeBoard";
+import { Button } from "@mantine/core";
+import { IconExternalLink } from "@tabler/icons";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
       <StatsGroup></StatsGroup>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: "50px"
+        }}
+      >
+        <Link to="/students/verified">
+        <Button 
+          rightIcon={<IconExternalLink />} 
+          size="lg"
+          >
+          Verified Students
+        </Button>
+        </Link>
+      </div>
       <NoticeBoard></NoticeBoard>
     </div>
   );
