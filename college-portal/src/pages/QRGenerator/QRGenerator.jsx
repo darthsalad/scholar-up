f803cec828a89813c9a23633487989a33a4fa1e1
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2022-08-24 19:16:24 +0530

    Added qr page

--- college-portal/src/pages/QRGenerator/QRGenerator.jsx
+++ college-portal/src/pages/QRGenerator/QRGenerator.jsx
@@ -1,29 +1,138 @@
-import { Button, Text } from "@mantine/core";
+import { useEffect, useState, useCallback } from "react";
+import downloadjs from "downloadjs";
+import html2canvas from "html2canvas";
+import { Button, Text, LoadingOverlay, Alert } from "@mantine/core";
+import { collection, query, where, getDocs, addDoc } from "firebase/firestore";
+import { auth, db } from "../../firebase.config";
+import { useAuthState } from "react-firebase-hooks/auth";
 import QRCode from "react-qr-code";
 import Navbar from "../../components/Navbar/Navbar";
 import { useStyles } from "./QRGenerator.styles";
+import { IconAlertCircle } from "@tabler/icons";
 
 const QRGenerator = () => {
   const { classes } = useStyles();
+  const [user, wait] = useAuthState(auth);
+  const [token, setToken] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!user) return;
+
+    async function getQRCode() {
+      try {
+        const q = query(
+          collection(db, "QRTokens"),
+          where("cdomain", "==", user.email.split("@")[1])
+        );
+
+        const querySnapshot = await getDocs(q);
+        querySnapshot.docs.forEach((doc) => {
+          if (
+            doc.data().validDate.toDate().getDate() === new Date().getDate()
+          ) {
+            setToken(doc.data().token);
+          }
+        });
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    getQRCode();
+  }, [user]);
+
+  const handleGenerate = async () => {
+    try {
+      setLoading(true);
+      const randomString = makeid(10);
+
+      await addDoc(collection(db, "QRTokens"), {
+        token: randomString,
+        cdomain: user.email.split("@")[1],
+        validDate: new Date(),
+      });
+
+      setToken(randomString);
+    } catch (err) {
+      console.log(err);
+    }
+    setLoading(false);
+  };
+
+  const handleCaptureClick = useCallback(async () => {
+    const canvas = await html2canvas(document.getElementById("qrcode"));
+    const dataURL = canvas.toDataURL("image/png");
+    downloadjs(
+      dataURL,
+      `qr-code-${new Date().toJSON().slice(0, 10).replace(/-/g, "/")}.png`,
+      "image/png"
+    );
+  }, []);
 
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar />
       <Text className={classes.text}>
         Generate QR for {new Date().toJSON().slice(0, 10).replace(/-/g, "/")}
       </Text>
+
+      {!loading && !token && (
+        <Button className={classes.btn} onClick={handleGenerate}>
+          Generate QR Code
+        </Button>
+      )}
+
+      {!loading && token && (
+        <Button className={classes.btn} onClick={handleCaptureClick}>
+          Capture QR Code
+        </Button>
+      )}
+
       <div className={classes.main}>
-        <div className={classes.qr}>
-          <QRCode
-            style={{ display: "block" }}
-            title="qr code"
-            value="https://mantine.dev/"
-          />
+        <div className={classes.wrapper}>
+          <div id="qrcode" className={classes.root}>
+            <div style={{ position: "relative" }}>
+              <LoadingOverlay visible={loading} overlayBlur={2} />
+              <div className={classes.qr}>
+                {token && (
+                  <QRCode
+                    style={{ display: "block" }}
+                    title="qr code"
+                    value={token}
+                  />
+                )}
+                {!loading && !token && (
+                  <Alert
+                    icon={<IconAlertCircle size={16} />}
+                    title="Oops!"
+                    color="red"
+                    radius="xs"
+                    variant="filled"
+                  >
+                    No QR code found. Generate a new qr code
+                  </Alert>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
-        <Button fullWidth>Generate QR Code</Button>
       </div>
     </>
   );
 };
+
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 export default QRGenerator;
--- college-portal/src/pages/QRGenerator/QRGenerator.styles.js
+++ college-portal/src/pages/QRGenerator/QRGenerator.styles.js
@@ -2,13 +2,56 @@ import { createStyles } from "@mantine/core";
 
 export const useStyles = createStyles((theme) => ({
   main: {
-    height: "fit-content",
-    width: "50vw",
-    minWidth: "fit-content",
-    margin: "auto",
-    border: `solid 2px ${theme.colors[theme.primaryColor][4]}`,
+    position: "absolute",
+    maxWidth: "80vw",
+    minWidth: "50vw",
+    margin: " 5rem 50%",
+    minHeight: "30rem",
+    transform: "translateX(-50%)",
+  },
+
+  wrapper: {
+    padding: "0.3rem",
     borderRadius: theme.radius.md,
-    padding: "2rem",
+    maxWidth: "80vw",
+    minHeight: "30rem",
+
+    "&:before": {
+      content: '""',
+      zIndex: -1,
+      position: "absolute",
+      top: "0",
+      right: "0",
+      bottom: "0",
+      left: "0",
+      background: `linear-gradient(-22deg, ${
+        theme.colors[theme.primaryColor][4]
+      } 11%, ${theme.colors[theme.primaryColor][7]} 125% )`,
+      transform: "translate3d(0px, -1px, 0) scale(1.02)",
+      filter: "blur(42px)",
+      opacity: "var(0.5)",
+      transition: "opacity 0.3s",
+      borderRadius: "inherit",
+    },
+
+    "&::after": {
+      content: '""',
+      zIndex: -1,
+      position: "absolute",
+      top: "0",
+      right: "0",
+      bottom: "0",
+      left: "0",
+      background: "inherit",
+      borderRadius: "inherit",
+    },
+  },
+
+  root: {
+    background: "white",
+    borderRadius: theme.radius.md,
+    minHeight: "30rem",
+    padding: "5rem",
   },
 
   text: {
@@ -26,6 +69,11 @@ export const useStyles = createStyles((theme) => ({
   qr: {
     background: "white",
     maxWidth: "fit-content",
+    margin: "2rem auto",
+  },
+  btn: {
+    display: "flex",
+    justifyContent: "center",
     margin: "2rem auto",
   },
 }));
