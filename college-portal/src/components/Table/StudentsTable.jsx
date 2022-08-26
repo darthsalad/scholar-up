4febbd645218478dc8ce486ad16fc362f8b2cf26
DarthSalad <piyushmishra965@gmail.com>
2022-08-26 14:39:50 +0530

    csv download link

--- college-portal/src/components/Table/StudentsTable.jsx
+++ college-portal/src/components/Table/StudentsTable.jsx
@@ -1,4 +1,4 @@
-import {Text, Table} from '@mantine/core';
+import {Text, Table, Button} from '@mantine/core';
 import React, { useEffect, useState } from "react";
 import { auth } from "../../firebase.config";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -6,6 +6,7 @@ import { getColleges, getStudents, getTotalClasses } from '../../api/profile.api
 import Load from '../Load/Load';
 import { useStyle } from "../../pages/verify/unverified.styles";
 import Navbar from '../Navbar/Navbar';
+import { CSVLink } from 'react-csv';
 
 const StudentsTable = () => {
   const { classes } = useStyle();
@@ -14,14 +15,37 @@ const StudentsTable = () => {
   const [students, setStudents] = useState(null);
   const [colleges, setColleges] = useState(null);
   const [totalClasses, setTotalClasses] = useState(null);
+  const [list, setList] = useState(null);
 
   useEffect(() => {
     user && getStudents(setStudents)
     && getColleges(setColleges)
     && getTotalClasses(setTotalClasses);
     setLoading(false);
-    // console.log(totalClasses);
   }, [user])
+
+  const headers = [
+    { label: "Name", key: "name" },
+    { label: "Email", key: "email" },
+    { label: "College", key: "college" },
+    { label: "Total Classes", key: "totalClass" },
+    { label: "Attended Classes", key: "attendance" },
+    { label: "Attendance %", key: "percentage" },
+  ];
+  
+  const generateList = () => {
+    setList(students.map((student) => (
+      {
+        name: student.student.sname,
+        email: student.student.email,
+        college: (colleges.find(college => college.domain === student.student.cdomain)).cname,
+        totalClass: (totalClasses.find(tot => tot.cdomain === student.student.cdomain)).totalClasses,
+        attendance: student.student.totalAtt,
+        percentage: ((student.student.totalAtt / (totalClasses.find(tot => tot.cdomain === student.student.cdomain)).totalClasses) * 100).toFixed(3)
+      }
+      )));
+      console.log(list);
+  }
 
   if (loading || !students || !colleges || !totalClasses) return <Load />  
 
@@ -31,7 +55,21 @@ const StudentsTable = () => {
       <Text className={classes.text}>Students Table</Text>
       <div>
         <div>
-
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "50px"
+          }}  
+        >
+          <Button onClick={() => {generateList()}}>
+            Generate CSV
+          </Button>
+          {list &&
+              <CSVLink style={{paddingLeft: "50px"}} data={list} headers={headers}><Button>Download</Button></CSVLink>
+          }
+        </div>
         </div>
       <Table striped highlightOnHover>
         <thead>
